402e4f4ce66955a4a00011d43e400f0ffbb7599f
Thanus Kumaar <111196217+Thanus-Kumaar@users.noreply.github.com>
2024-04-07 00:21:25 +0530

    Update constants.js

--- frontend/public/constants.js
+++ frontend/public/constants.js
@@ -1,2 +1,2 @@
-const NGROK_TUNNEL = "https://ade7-2409-408d-3e07-e09b-cc29-a159-d14e-453e.ngrok-free.app"
+const NGROK_TUNNEL = "https://d31b-2409-408d-495-2157-902f-da8f-7ce4-afd0.ngrok-free.app"
 export default NGROK_TUNNEL
